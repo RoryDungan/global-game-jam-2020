2d3db996bb079f3e29b7944ce356f93e09bfa4ba
Rory Dungan <r.dungan@chaostheorygames.com>
2020-02-02 10:50:22 +1100

    Updates

---
+++ precache-manifest.ba5b24bf4e30aa59b0bbab9ee96221bb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea08ff1602053bedb4ac6b9f4d1cfe7",
+    "url": "/global-game-jam-2020/index.html"
+  },
+  {
+    "revision": "1f28136eb9326315fae6",
+    "url": "/global-game-jam-2020/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b2044ef6e0973e66e1dc",
+    "url": "/global-game-jam-2020/static/css/main.e1c5eac0.chunk.css"
+  },
+  {
+    "revision": "1f28136eb9326315fae6",
+    "url": "/global-game-jam-2020/static/js/2.8b10784e.chunk.js"
+  },
+  {
+    "revision": "0db3c3c9fc917cd7e5377649ab44c454",
+    "url": "/global-game-jam-2020/static/js/2.8b10784e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2044ef6e0973e66e1dc",
+    "url": "/global-game-jam-2020/static/js/main.eca1c268.chunk.js"
+  },
+  {
+    "revision": "e745d900185c00fd768e",
+    "url": "/global-game-jam-2020/static/js/runtime-main.6427e579.js"
+  },
+  {
+    "revision": "5a71d76f28fcae0e7a5c61c2ef6a18e6",
+    "url": "/global-game-jam-2020/static/media/avatar.5a71d76f.png"
+  },
+  {
+    "revision": "846763c2f96a29d2a9a18c0bea7bc1c0",
+    "url": "/global-game-jam-2020/static/media/hitBottomMessageLog.846763c2.m4a"
+  },
+  {
+    "revision": "fc2d7e8b38b4c26f7c0511583b4186e2",
+    "url": "/global-game-jam-2020/static/media/hitTopMessageLog.fc2d7e8b.m4a"
+  },
+  {
+    "revision": "b57fdabfbf212cbfe6655c168486b557",
+    "url": "/global-game-jam-2020/static/media/notification.b57fdabf.m4a"
+  },
+  {
+    "revision": "8cc0cd6e4cadcc6116db892a5fe4e6a2",
+    "url": "/global-game-jam-2020/static/media/receiveMessage.8cc0cd6e.m4a"
+  },
+  {
+    "revision": "20d0835e80613511b774a9072e735223",
+    "url": "/global-game-jam-2020/static/media/selection.20d0835e.m4a"
+  },
+  {
+    "revision": "dbada8f44b2381c6a06897e9e0bd8ef6",
+    "url": "/global-game-jam-2020/static/media/sendMessage.dbada8f4.m4a"
+  },
+  {
+    "revision": "1461d7d53538d77457a9cf8cc1e41761",
+    "url": "/global-game-jam-2020/static/media/themeSong.1461d7d5.m4a"
+  },
+  {
+    "revision": "5e49bb5f5a2b3d54cc8d5d882da38283",
+    "url": "/global-game-jam-2020/static/media/themeSong2.5e49bb5f.m4a"
+  }
+]);
